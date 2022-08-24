24caf8c14d9862cabc88d336d45947afc91db9f6
Russell Ingram <brontosauruss@gmail.com>
2022-08-24 18:52:49 -0500

    Basic skeleton and functionality, will add styling and flesh out details later

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Container } from '@mui/system'
+import { Card, Divider, TextField, Typography } from '@mui/material'
+import wordBank from './assets/wordBank';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState('');
+
+  // TODO: useReducer?
+  const [timer, setTimer] = useState(60);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  // TODO: Randomize the wordBank
+  const [nextWordUp, setNextWordUp] = useState(wordBank[0]);
+  const [wordCount, setWordCount] = useState(0);
+
+  const startTimer = () => {
+    // setTimerStarted(true);
+    // setInterval(() => {
+    //   console.log("TEST");
+    //   if (timer > 0) {
+    //     setTimer(timer - 1);
+    //   }
+    // }, 1000)
+  }
+
+  const stopTimer = () => {
+    // change component view to results
+    // maybe tuck away results for comparison on future attempts - use localStorage?
+  }
+
+  useEffect(() => {
+    if (userInput === `${nextWordUp} `) { // adding a space
+      // TODO: maybe combine this using useReducer?
+      setNextWordUp(wordBank[wordCount + 1]);
+      setWordCount(wordCount + 1);
+      setUserInput('');
+    }
+
+    if (!timerStarted) {
+      startTimer();
+    }
+  }, [userInput])
+
+  const splitUserInput = userInput.split('');
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Card>
+          <Typography variant="body2">{wordBank}</Typography>
+          <Divider />
+          <Typography variant="body2">
+            { nextWordUp.split('').map((letter, index) => {
+              // TODO: add keys to this map
+              return letter === splitUserInput[index] ? (
+                <b>{letter}</b>
+              ) : (
+                // TODO: add css for wrong letters
+                // TODO: add logic for formatting for extra letters beyond first wrong one
+                <span>{letter}</span>
+              )
+            })}
+          </Typography>
+          <Divider />
+          <Typography variant="body2">WordCount: {wordCount}</Typography>
+          <Divider />
+          <Typography variant="body2">Timer: {timer}</Typography>
+          <Divider />
+          <TextField value={userInput} onChange={(e) => { setUserInput(e.target.value) }}></TextField>
+        </Card>
+      </Container>
     </div>
   )
 }
